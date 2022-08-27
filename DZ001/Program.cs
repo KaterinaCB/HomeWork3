e6055e25f926372970e46d149a22fe48d4b59867
ceb <vivu1991@mail.ru>
2022-08-27 18:15:46 +0300

    DZ 001 решение первой задачи

---
+++ DZ001/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите пятизначиное целое число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int a = num % 10;
+int b = (num / 10000) % 10;
+int c = (num / 10) % 10;
+int d = (num / 1000) % 10;
+
+if (a == b & c == d) Console.WriteLine("Да");
+else Console.WriteLine("Нет");
