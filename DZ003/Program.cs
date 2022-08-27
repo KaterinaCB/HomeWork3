0a7cbc92a3ed3c4da13cb1904d00acdddc4e0f3c
ceb <vivu1991@mail.ru>
2022-08-27 19:00:33 +0300

    DZ 003 решение третьей задачи

---
+++ DZ003/Program.cs
@@ -0,0 +1,23 @@
+﻿string NumbersInCube()
+{
+    Console.Write("Введите целое число ");
+    int N = Convert.ToInt32(Console.ReadLine());
+    int i = 1;
+    string result = string.Empty;
+
+    while (i <= N)
+    {
+        result = result + Math.Pow(i,3) + ",";
+        i = i + 1;
+    }
+    return result;
+}
+
+try
+{
+    Console.WriteLine(NumbersInCube());
+}
+catch
+{
+    Console.WriteLine("Ошибка ввода! Введите целое число");
+}
